b11bfb617f44d511f0161778033248974dd0df0c
sting800 <sting800@users.noreply.github.com>
2014-10-31 17:53:49 +0800

    Age of Laika
    Initial upload

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsText: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var convertRealButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let yearsFromText = humanYearsText.text.toInt()!
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(yearsFromText * 7)" + " dog years"
+        
+        humanYearsText.resignFirstResponder()
+        
+    }
 
+    @IBAction func convertRealButtonPressed(sender: UIButton) {
+        let yearsFromRealText = humanYearsText.text
+        let optionalDoubleFromRealText = Double((yearsFromRealText as NSString).doubleValue)
+        dogYearsLabel.hidden = false
+        if optionalDoubleFromRealText <= 2 {
+            dogYearsLabel.text = "\(optionalDoubleFromRealText * 10.5)" + " real dog years"
+        }
+        else {
+            dogYearsLabel.text = "\(21 + ((optionalDoubleFromRealText - 2) * 4))" + " real dog years"
+        }
+        
+        humanYearsText.resignFirstResponder()
+
+    }
 }
 
